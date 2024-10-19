43005bc35b4df2899f0f1cfd4ec2a5e29ecdb467
MJAZ93 <amatlhombe@gmail.com>
2024-10-19 15:31:47 +0200

    fix: remote scripts

--- build/remote-windows.ps1
+++ build/remote-windows.ps1
@@ -48,13 +48,27 @@ try {
 }
 
 # Example command execution
-Write-Host "Running installation commands..."
-$installScript = "./install.ps1"
-if (-not (Test-Path $installScript)) {
-    exit_on_error "install.ps1 script not found"
+# PowerShell script for Windows
+
+# Output message
+Write-Host "Running anto with 'init' parameter on Windows..."
+
+# Check if the __MACOSX folder exists and remove it if it does
+if (Test-Path "__MACOSX") {
+    Write-Host "Removing __MACOSX..."
+    Remove-Item -Recurse -Force "__MACOSX"
 }
-& $installScript
-if (!$?) { exit_on_error "Failed to run install.ps1" }
+
+# Navigate to the extracted folder
+Set-Location ".anto" -ErrorAction Stop
+
+# Windows - Run the 'anto' binary with 'init' parameter
+Write-Host "Running anto with 'init' parameter on Windows..."
+# No need to set execute permission on Windows, just run the executable
+Start-Process -NoNewWindow -Wait "./anto.exe" "init"
+
+Write-Host "Installation complete!"
+
 
 # Clean up
 Write-Host "Cleaning up..."
